ed5cbc860299bb9ef6386a2c70d289d1c00aa024
Howard <dryadd44651@gmail.com>
2024-11-03 14:22:57 -0800

    update Navigator

--- frontend/fitness-website/src/App.js
+++ frontend/fitness-website/src/App.js
@@ -2,25 +2,20 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import TrainingPage from './components/TrainingPage';
 import ProfilePage from './components/ProfilePage';
+import './index.css'; // 引入更新的 CSS 文件
 
 function App() {
   return (
     <Router>
       <div>
-        {/* 可以加入導航列 */}
         <nav>
           <a href="/Training">Training</a>
           <a href="/Profile">Profile</a>
         </nav>
 
         <Routes>
-          {/* 訓練頁面路由 */}
           <Route path="/Training" element={<TrainingPage />} />
-          
-          {/* Profile頁面路由 */}
           <Route path="/Profile" element={<ProfilePage />} />
-
-          {/* 預設頁面，可選擇設定為首頁 */}
           <Route path="/" element={<h1>Welcome to the Fitness App</h1>} />
         </Routes>
       </div>
